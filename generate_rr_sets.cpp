2d21ce18a15a198c2d55dd4ee269b294649d32a6
Dylan Huang <dylan.p.huang@gmail.com>
2018-11-14 20:35:25 -0600

    Comitting .cpp code

---
+++ generate_rr_sets.cpp
@@ -0,0 +1,61 @@
+#include <curand_kernel.h>
+
+typedef struct
+{
+    int id;
+    node *prev;
+    node *next;
+    node(int id) : id(id){};
+} node;
+
+__global__ void init_rng(int nthreads, curandState *states, unsigned long long seed, unsigned long long offset)
+{
+    int id = blockIdx.x * blockDim.x + threadIdx.x;
+    if (id >= nthreads)
+        return;
+    /* Each thread gets same seed, a different sequence number, no offset */
+    curand_init(seed, id, offset, &state[id]);
+}
+
+__global__ void generate_rr_sets(float *data, int *rows, int *cols, int *out, int numNodes, int numNonZeros, int theta, curandState *states)
+{
+    const unsigned int tid = blockDim.x * blockIdx.x + threadIdx.x;
+    int * rr_set = out[tid];
+    if (tid < theta)
+    {
+        curandState state = states[tid];
+
+        /* Because C does not give us the luxury of dynamic arrays, to imitate the
+        behavior of a stack, I am using a linked list*/
+        int randomNodeId = ceil(numNodes * curand_uniform(&state)) - 1;
+        node *stack = new node(randomNodeId);
+
+        // Returns false when stack is NULL
+        while (stack)
+        {
+            // pop from stack
+            int currentNodeId = stack->id;
+            node *temp = stack;
+            stack = stack->prev;
+            free(temp);
+
+            // If current is not in visited
+            if (!rr_set[currentNodeId])
+            {
+                rr_set[currentNodeId] = 1; // visited.add(currentNodeId)
+
+                int dataStart = rows[currentNodeId];
+                int dataEnd = rows[currentNodeId + 1];
+
+                for (unsigned int i = dataStart; i < dataEnd; i++)
+                {
+                    if (curand_uniform(&state) < data[i])
+                    {
+                        // append to stack
+                        stack->next = new node(cols[i])
+                    }
+                }
+            }
+        }
+    }
+}
