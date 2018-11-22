ce490798cd6365c0b2c1969b58cc15ac869a3bed
Blue Waters Trainee 107 <tra107@h2ologin2.ncsa.illinois.edu>
2018-11-22 01:27:24 -0600

    Updated make.sh

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "Making serial.cpp"
-g++ -std=c++11 -g -o serial serial.cpp
+g++ -std=c++11 -g -o serial shared.cpp serial.cpp
 echo "Making parallel.cu"
-nvcc -std=c++11 -g -o parallel parallel.cu
+nvcc -std=c++11 -g -G -o parallel shared.cpp parallel.cu
