8e5aeba215ce8e21a7e219c7a0d4e7fcc37cce4f
Dylan Huang <dylan.p.huang@gmail.com>
2018-11-25 21:55:07 -0600

    Added analysis for serial vs. parallel

---
+++ analysis/log_to_csv.py
@@ -0,0 +1,37 @@
+import sys
+from collections import defaultdict
+
+def convert_to_csv(filename, num_trials):
+    csv = 'num_nodes'
+
+    with open(filename, 'r') as fp:
+        lines = fp.read().split('\n')
+
+    for i in range(num_trials):
+        csv += ',trial {}'.format(i)
+    csv += '\n'
+
+    data = defaultdict(list) # Map from number of nodes to time
+    current_num_nodes = -1
+    for line in lines:
+        if 'Trial' in line:
+            current_num_nodes = int(line.split('.')[0].split('_')[-1])
+        elif 'findKSeeds' in line:
+            data[current_num_nodes].append(float(line.split(' ')[-1]) * 1e-6)
+
+    for num_nodes in sorted(data.keys()):
+        csv += str(num_nodes)
+        for trial in data[num_nodes]:
+            csv += ',{}'.format(trial)
+        csv += '\n'
+
+    return csv
+
+if __name__=="__main__":
+    if len(sys.argv) != 3:
+        print 'Pass in path of .log file and # of trials'
+        exit()
+    filepath = sys.argv[1]
+    num_nodes = int(sys.argv[2])
+    with open(filepath.split('.')[0] + '.csv', 'w') as fp:
+        fp.write(convert_to_csv(filepath, num_nodes))
